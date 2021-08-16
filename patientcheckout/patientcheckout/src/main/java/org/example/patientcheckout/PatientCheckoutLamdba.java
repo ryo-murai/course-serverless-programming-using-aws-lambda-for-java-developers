861bcfad6030b755137e353b1a0c938610e6730e
ryo-murai <murai.ry@gmail.com>
2021-08-16 13:30:24 +0900

    add logs using lambdalogger

--- patientcheckout/patientcheckout/src/main/java/org/example/patientcheckout/PatientCheckoutLamdba.java
+++ patientcheckout/patientcheckout/src/main/java/org/example/patientcheckout/PatientCheckoutLamdba.java
@@ -1,6 +1,7 @@
 package org.example.patientcheckout;
 
 
+import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
@@ -10,6 +11,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Arrays;
 
 public class PatientCheckoutLamdba {
@@ -18,7 +21,8 @@ public class PatientCheckoutLamdba {
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final AmazonSNS sns = AmazonSNSClientBuilder.defaultClient();
 
-    public void handler(S3Event event) {
+    public void handler(S3Event event, Context context) {
+        var logger = context.getLogger();
         event.getRecords().forEach(record -> {
             var s3Entry = record.getS3();
             try (var s3inputStream =
@@ -26,17 +30,21 @@ public class PatientCheckoutLamdba {
                     .getObjectContent() ) {
                 var patientCheckoutEvents =
                         Arrays.asList(objectMapper.readValue(s3inputStream, PatientCheckoutEvent[].class));
-                System.out.println(patientCheckoutEvents);
+                logger.log(patientCheckoutEvents.toString());
 
                 patientCheckoutEvents.forEach(checkoutEvent -> {
                     try {
                         sns.publish(PATIENT_CHECKOUT_TOPIC, objectMapper.writeValueAsString(checkoutEvent));
                     } catch (JsonProcessingException e) {
-                        e.printStackTrace();
+                        var writer = new StringWriter();
+                        e.printStackTrace(new PrintWriter(writer));
+                        logger.log(writer.toString());
                     }
                 });
             } catch (IOException e) {
-                e.printStackTrace();
+                var writer = new StringWriter();
+                e.printStackTrace(new PrintWriter(writer));
+                logger.log(writer.toString());
             }
         });
     }
